7642b1e826c2508d5fdf149f984a152f8caa1ef0
EDWARD CHAN <edward.chan@radialpoint.com>
2015-10-26 21:47:43 -0400

    Updated android example. Can click on button to open conversation window.

--- example/android/assets/www/js/index.js
+++ example/android/assets/www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        document.getElementById('myBtn').addEventListener('click', SupportKit.showConversation, false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
